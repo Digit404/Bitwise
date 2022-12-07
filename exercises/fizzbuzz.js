72cf848b87dcd84d588cd04290517056e56c9c7f
Digit <digit.10k@gmail.com>
2022-12-07 10:59:19 -0600

    added garbage

---
+++ exercises/fizzbuzz.js
@@ -0,0 +1,16 @@
+for (var i = 1; i <= 100; i++){
+    document.write('<p class="text" style="margin-top:0;">')
+    if(i % 3 === 0 && i % 5 === 0){
+        document.write("FIZZBUZ");
+    }
+    else if (i % 3 === 0){
+        document.write("FIZZ")
+    }
+    else if (i % 5 === 0){
+        document.write("BUZZ")
+    }
+    else{
+        document.write(i)
+    }
+    document.write("<br></p>")
+}
---
+++ exercises/login.js
@@ -0,0 +1,23 @@
+var attempts = 0;
+
+function login(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    if(username === "username" && password === "password" && attempts < 3){
+        document.getElementById("beans").innerHTML = "BEANS";
+        document.getElementById("whatwhat").innerHTML = "OKAY!"
+    }
+    else{
+        if(attempts >= 3){
+            document.getElementById("beans").innerHTML = "LOCKED";
+            document.getElementById("beans").style.color = "#ff2424"
+            document.getElementById("whatwhat").innerHTML = "LOCKED"
+            document.getElementById("whatwhat").style.backgroundColor = "#FFFFFFA0";
+        }
+        else {
+            attempts++;
+        }
+        document.getElementById("whatwhat").innerHTML = 3 - attempts + " ATTEMPTS REMAINING";
+    }
+}
