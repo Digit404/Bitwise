c3d1449f528ede44120ee2afc5322b3bb6e67199
Digit <digit.10k@gmail.com>
2022-12-07 14:40:05 -0600

    improvements, got rid of scrollbar

--- exercises/calculator.js
+++ exercises/calculator.js
@@ -6,81 +6,81 @@ let waitingForSecondOperand = false; // flag for when the calculator is waiting 
 
 // Helper function to update the calculator screen
 function updateDisplay() {
-  document.getElementById("thDisplay").innerText = displayValue;
+    document.getElementById("thDisplay").innerText = displayValue;
 }
 
 // Function to reset the calculator
 function Reset() {
-  displayValue = "0";
-  firstOperand = null;
-  operator = null;
-  waitingForSecondOperand = false;
-  updateDisplay();
+    displayValue = "0";
+    firstOperand = null;
+    operator = null;
+    waitingForSecondOperand = false;
+    updateDisplay();
 }
 
 // Function to handle clicks on the number buttons
 function ClickNumber(btn) {
-  let btnValue = btn.innerText;
+    let btnValue = btn.innerText;
 
-  if (waitingForSecondOperand === true) {
-    // if the calculator is waiting for the second operand, reset the display and set the waiting flag to false
-    displayValue = btnValue;
-    waitingForSecondOperand = false;
-  } else {
-    // if the current display value is "0", replace it with the clicked number
-    // otherwise, concatenate the clicked number to the current display value
-    displayValue = displayValue === "0" ? btnValue : displayValue + btnValue;
-  }
+    if (waitingForSecondOperand === true) {
+        // if the calculator is waiting for the second operand, reset the display and set the waiting flag to false
+        displayValue = btnValue;
+        waitingForSecondOperand = false;
+    } else {
+        // if the current display value is "0", replace it with the clicked number
+        // otherwise, concatenate the clicked number to the current display value
+        displayValue = displayValue === "0" ? btnValue : displayValue + btnValue;
+    }
 
-  updateDisplay();
+    updateDisplay();
 }
 
 // Function to handle clicks on the operator buttons
 function ClickOperator(btn) {
-  let btnValue = btn.innerText;
+    let btnValue = btn.innerText;
 
-  // if the calculator is waiting for the second operand, set the operator and return
-  if (waitingForSecondOperand === true) {
+    // if the calculator is waiting for the second operand, set the operator and return
+    if (waitingForSecondOperand === true) {
+        operator = btnValue;
+        return;
+    }
+
+    // if the first operand is null, set it to the current display value
+    if (firstOperand === null) {
+        firstOperand = parseFloat(displayValue);
+    } else {
+        // if the first operand and operator are not null, perform the calculation and update the display value
+        let result = performCalculation(firstOperand, operator, parseFloat(displayValue));
+        displayValue = String(result);
+        firstOperand = result;
+    }
+
+    // set the operator and waiting flag
     operator = btnValue;
-    return;
-  }
+    waitingForSecondOperand = true;
 
-  // if the first operand is null, set it to the current display value
-  if (firstOperand === null) {
-    firstOperand = parseFloat(displayValue);
-  } else {
-    // if the first operand and operator are not null, perform the calculation and update the display value
-    let result = performCalculation(firstOperand, operator, parseFloat(displayValue));
-    displayValue = String(result);
-    firstOperand = result;
-  }
-
-  // set the operator and waiting flag
-  operator = btnValue;
-  waitingForSecondOperand = true;
-
-  updateDisplay();
+    updateDisplay();
 }
 
 // Function to handle the "Flip" button
 function Flip() {
-  // if the current display value is not "0", multiply it by -1
-  if (displayValue !== "0") {
-    displayValue = String(parseFloat(displayValue) * -1);
-  }
+    // if the current display value is not "0", multiply it by -1
+    if (displayValue !== "0") {
+        displayValue = String(parseFloat(displayValue) * -1);
+    }
 
-  updateDisplay();
+    updateDisplay();
 }
 
 // Helper function to perform the calculation
 function performCalculation(firstOperand, operator, secondOperand) {
-  if (operator === "+") {
-    return firstOperand + secondOperand;
-  } else if (operator === "-") {
-    return firstOperand - secondOperand;
-  } else if (operator === "X") {
-    return firstOperand * secondOperand;
-  } else if (operator === "/") {
-    return firstOperand / secondOperand;
-  }
+    if (operator === "+") {
+        return firstOperand + secondOperand;
+    } else if (operator === "-") {
+        return firstOperand - secondOperand;
+    } else if (operator === "X") {
+        return firstOperand * secondOperand;
+    } else if (operator === "/") {
+        return firstOperand / secondOperand;
+    }
 }
