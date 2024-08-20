76b176117dc626911dd38e985474383c24818923
Digit404 <digit.10k@gmail.com>
2024-08-20 14:00:33 -0700

    added nav, reworked styles

--- blog/dark-mode-button.js
+++ blog/dark-mode-button.js
@@ -15,3 +15,18 @@ function toggleDarkMode() {
 }
 
 darkModeButton.addEventListener("click", toggleDarkMode);
+
+if (document.querySelector("nav")) {
+    const hamburgerButton = document.createElement("div");
+    hamburgerButton.id = "hamburger-button";
+    hamburgerButton.textContent = "menu";
+    document.querySelector("nav").prepend(hamburgerButton);
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const navLinks = document.querySelector('nav ul');
+
+        hamburgerButton.addEventListener('click', () => {
+            navLinks.style.display = navLinks.style.display === 'block' ? 'none' : 'block';
+        });
+    });
+}
