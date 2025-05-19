67516665ba5b0107361acea20f8eb5a9e51cb2ea
Digit <digit.10k@gmail.com>
2025-05-19 13:13:33 -0700

    updated styles for list formatter

--- tools/list-formatter/list-maker.js
+++ tools/list-formatter/list-maker.js
@@ -1,21 +1,18 @@
 // Get references to all the input and output elements in the HTML
-var inputBox = document.getElementById("input");
-var outputBox = document.getElementById("output");
-var quoteCheck = document.getElementById("quotes");
-var mlCheck = document.getElementById("multiline");
-var advCheck = document.getElementById("checkAdv");
-var quotesCheck = document.getElementById('quotes');
-var numQuoteCheck = document.getElementById('numQuote');
-var wordWrapCheck = document.getElementById('wordWrap');
-var advOptions = document.getElementById("advOptions");
-var mathCheck = document.getElementById("numMath");
-var inputDelimCheck = document.getElementById("inputDelimCheck");
-var inputDelimBox = document.getElementById("inputDelimBox");
-var showDelimCheck = document.getElementById("showDelimCheck");
-var delimOptions = document.getElementById("delimOptions");
-var outDelimBox = document.getElementById("outDelimBox");
+const inputBox = document.getElementById("input");
+const outputBox = document.getElementById("output");
+const quotesCheck = document.getElementById("quotes-check");
+const mlCheck = document.getElementById("multiline-check");
+const rawCheckbox = document.getElementById("raw-check");
+const wordWrapCheckbox = document.getElementById("word-wrap-check");
+const mathCheck = document.getElementById("numMath");
+const inputDelimCheck = document.getElementById("in-delim-check");
+const inputDelimBox = document.getElementById("in-delim-box");
+const showDelimCheck = document.getElementById("in-delim-check");
+const outDelimBox = document.getElementById("out-delim-box");
+const copyButton = document.getElementById("copy-button");
 
-var outDelim = ", "
+let outDelim = ", ";
 
 // Function to word wrap the text to 80 characters per line
 function wordWrap(text) {
@@ -39,11 +36,11 @@ function wordWrap(text) {
         }
     }
     return output;
-};
+}
 
 // Main format function that is called on an interval
 function format() {
-    var text = [inputBox.value];
+    let text = [inputBox.value];
     let delimiters = inputDelimBox.value.split("|").concat(["\n", ", ", ","]); // get all delimiters separated by "|"
 
     outDelim = outDelimBox.value; // set the output delimiters
@@ -52,85 +49,69 @@ function format() {
     for (let i = 0; i < delimiters.length; i++) {
         // for each of the delimiters
         if (delimiters[i] !== "") {
-            text = text.flatMap(str => str.split(delimiters[i])); // split on each of the delimiters, this is how you flatten the list.
+            text = text.flatMap((str) => str.split(delimiters[i])); // split on each of the delimiters, this is how you flatten the list.
         }
     }
 
     // If the quote checkbox is checked, add quotes around each string
-    if (quoteCheck.checked) {
+    if (quotesCheck.classList.contains("checked")) {
         for (let i = 0; i < text.length; i++) {
             // If the "exclude numbers" checkbox is checked and the current string is a number,
             // don't add quotes around it
-            if (!(numQuoteCheck.checked && quoteCheck.checked && !isNaN(text[i]))) {
+            if (!(rawCheckbox.classList.contains("checked") && quotesCheck.classList.contains("checked") && !isNaN(text[i]))) {
                 text[i] = '"' + text[i] + '"';
             }
         }
     }
 
     // If the word wrap checkbox is checked, use the word wrap function to format the text
-    if (wordWrapCheck.checked && mlCheck.checked) {
+    if (wordWrapCheckbox.classList.contains("checked") && mlCheck.classList.contains("checked")) {
         text = wordWrap(text);
     }
     // If the multiline checkbox is checked, format the text as a comma-separated list with each item on its own line
-    else if (mlCheck.checked) {
+    else if (mlCheck.classList.contains("checked")) {
         text = text.join(outDelim + "\n");
     } else {
         text = text.join(outDelim);
     }
 
     outputBox.value = text;
-};
+}
 
 // copy function for copying the text to clipboard
 function copy() {
     navigator.clipboard.writeText(outputBox.value);
-    document.getElementById("copyButton").innerHTML = "COPIED!";
+    copyButton.innerHTML = "check";
+    copyButton.classList.add("check");
 
     setTimeout(function () {
-        document.getElementById("copyButton").innerHTML = "COPY";
-    }, 3000); // Change the text back to "COPY" after 3000 milliseconds (3 second)
-};
+        copyButton.innerHTML = "content_copy";
+        copyButton.classList.remove("check");
+    }, 2000); // Change the text back to "COPY" after 3000 milliseconds (3 second)
+}
 
 window.copy = copy;
 
-// advanced options checkbox listener
-advCheck.addEventListener("change", function () {
-    if (!this.checked) {
-        advOptions.style.display = 'none';
-    } else {
-        advOptions.style.display = 'block';
-    }
-});
-
-// delim options checkbox listener
-showDelimCheck.addEventListener("change", function () {
-    if (!this.checked) {
-        delimOptions.style.display = 'none';
-    } else {
-        delimOptions.style.display = 'block';
-    }
-});
-
 // multiline checkbox listener
-mlCheck.addEventListener('change', function () {
-    if (this.checked) {
-        wordWrapCheck.disabled = false;
-        wordWrapCheck.style.opacity = 1;
-    } else {
-        wordWrapCheck.disabled = true;
-        wordWrapCheck.style.opacity = 0.5;
-    }
+mlCheck.addEventListener("click", function () {
+    this.classList.toggle("checked");
+    wordWrapCheckbox.classList.remove("checked");
+    wordWrapCheckbox.classList.toggle("disabled");
 });
 
 // quotes checkbox listener
-quotesCheck.addEventListener('change', function () {
-    if (this.checked) {
-        numQuoteCheck.disabled = false;
-        numQuoteCheck.style.opacity = 1;
-    } else {
-        numQuoteCheck.disabled = true;
-        numQuoteCheck.style.opacity = 0.5;
-    }
+quotesCheck.addEventListener("click", function () {
+    this.classList.toggle("checked");
+    rawCheckbox.classList.remove("checked");
+    rawCheckbox.classList.toggle("disabled");
 });
 
-setInterval(format, 10)
+rawCheckbox.addEventListener("click", function () {
+    this.classList.toggle("checked");
+});
+
+wordWrapCheckbox.addEventListener("click", function () {
+    this.classList.toggle("checked");
+});
+
+setInterval(format, 10);
